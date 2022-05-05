38b356b62ef803ac6b46f7e6f4cc17c712ab193f
Paul Odhiambo Gariy <Paul.O.Gariy-1@ou.edu>
2022-05-05 17:39:08 -0500

    Added Stem code NonogramPresenter

---
+++ src/edu/ou/cs2334/project5/presenters/NonogramPresenter.java
@@ -0,0 +1,24 @@
+package edu.ou.cs2334.project5.presenters;
+
+import java.io.File;
+import java.io.IOException;
+
+import edu.ou.cs2334.project5.handlers.OpenHandler;
+import edu.ou.cs2334.project5.interfaces.Openable;
+import edu.ou.cs2334.project5.models.CellState;
+import edu.ou.cs2334.project5.models.NonogramModel;
+import edu.ou.cs2334.project5.views.NonogramView;
+import javafx.scene.input.MouseButton;
+import javafx.scene.layout.Pane;
+import javafx.stage.FileChooser;
+import javafx.stage.Window;
+import javafx.stage.FileChooser.ExtensionFilter;
+
+public class NonogramPresenter implements Openable{
+	
+	private NonogramView view;
+	private NonogramModel model;
+	private int cellLength;
+	private final static String DEFAULT_PUZZLE = "puzzles/space-invader.txt";
+	
+}
