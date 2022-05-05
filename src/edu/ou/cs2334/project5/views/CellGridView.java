b3ebd3a14ed4c586c017a514bfffcbf669dfe44d
Paul Odhiambo Gariy <Paul.O.Gariy-1@ou.edu>
2022-05-05 17:19:04 -0500

    Implemented CellGridView ctor

--- src/edu/ou/cs2334/project5/views/CellGridView.java
+++ src/edu/ou/cs2334/project5/views/CellGridView.java
@@ -8,7 +8,11 @@ public class CellGridView extends GridPane{
 	private static final String STYLE_CLASS = "cell-grid-view";
 	private CellView[][] cellViews;
 	
-	
+	public CellGridView(int numRows, int numCols, int cellLength) {
+		initCells(numRows, numCols, cellLength);
+		this.getStyleClass().add(STYLE_CLASS);
+		
+	}
 	
 	
 }
