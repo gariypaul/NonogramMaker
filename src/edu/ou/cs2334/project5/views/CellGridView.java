62d1fb936dbc64c18ce4d828e89e7c601237a163
Paul Odhiambo Gariy <Paul.O.Gariy-1@ou.edu>
2022-05-05 17:18:25 -0500

    Wrote initial CellGridView Code

---
+++ src/edu/ou/cs2334/project5/views/CellGridView.java
@@ -0,0 +1,14 @@
+package edu.ou.cs2334.project5.views;
+
+import edu.ou.cs2334.project5.models.CellState;
+import javafx.geometry.Pos;
+import javafx.scene.layout.GridPane;
+
+public class CellGridView extends GridPane{
+	private static final String STYLE_CLASS = "cell-grid-view";
+	private CellView[][] cellViews;
+	
+	
+	
+	
+}
